afc1c49857e1ecc800c511cec7687a043166a518
dodoBrb <dorine.barbey.auditeur@lecnam.net>
2025-09-30 10:47:40 +0200

    Exercice 2

--- src/net/lecnam/ussi47/tp2/Exercice2.java
+++ src/net/lecnam/ussi47/tp2/Exercice2.java
@@ -3,7 +3,15 @@ package net.lecnam.ussi47.tp2;
 public class Exercice2 {
 
     public static void main(String[] args) {
-
+    int cote1 = 3 ;
+    int cote2 = 4 ;
+    int hypothenuse = 5 ;
+    if (hypothenuse*hypothenuse == (cote1*cote1)+(cote2*cote2)) {
+        System.out.println("Le théorème de Pythagore est vrai");
+    }
+    else{
+        System.out.println("Le théorème de Pythagore est faux");
+    }
 
     }
 }
