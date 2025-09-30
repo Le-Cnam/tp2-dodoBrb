caa6fd42b1b819cb40c80264bdbb7f843652fa4f
dodoBrb <dorine.barbey.auditeur@lecnam.net>
2025-09-30 11:18:05 +0200

    Exercice 4

--- src/net/lecnam/ussi47/tp2/Exercice4.java
+++ src/net/lecnam/ussi47/tp2/Exercice4.java
@@ -3,7 +3,10 @@ package net.lecnam.ussi47.tp2;
 public class Exercice4 {
 
     public static void main(String[] args) {
-
-
+    float facteurConversion = 3.28084f ;
+    int metres = 4000 ;
+    float conversion = facteurConversion * metres ;
+    System.out.println("Le facteur de conversion est de " + facteurConversion);
+    System.out.println("La hauteur en pied est de " + conversion);
     }
 }
