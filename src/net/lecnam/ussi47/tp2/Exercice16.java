1b7bae28e24560b14806f6498648d1100f8d61cd
dodoBrb <dorine.barbey.auditeur@lecnam.net>
2025-09-30 12:16:23 +0200

    Exercice 16

--- src/net/lecnam/ussi47/tp2/Exercice16.java
+++ src/net/lecnam/ussi47/tp2/Exercice16.java
@@ -3,7 +3,19 @@ package net.lecnam.ussi47.tp2;
 public class Exercice16 {
 
     public static void main(String[] args) {
-
-
+        int x = 5 ;
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= x - i; j++) {
+                System.out.print(" ") ;
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                if (k == 1 || k == 2 * i - 1 || i == x) {
+                    System.out.print("*") ;
+                } else {
+                    System.out.print(" ") ;
+                }
+            }
+            System.out.println() ;
+        }
     }
 }
